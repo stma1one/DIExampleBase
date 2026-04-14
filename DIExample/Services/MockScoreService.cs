4b0aaadff7aeb3dbab4d73b1868b2d9445b6a722
Owner <stam1@gmail.com>
2026-04-14 13:17:05 +0300

    add Mock

---
+++ DIExample/Services/MockScoreService.cs
@@ -0,0 +1,22 @@
+﻿using DIExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIExample.Services
+{
+	internal class MockScoreService 
+	{
+		public List<PlayerScore> GetHighScores()
+		{
+			return new List<PlayerScore>
+		{
+			new PlayerScore { Name = "רון", Score = 150 },
+			new PlayerScore { Name = "מיכל", Score = 120 },
+			new PlayerScore { Name = "דני", Score = 90 }
+		};
+		}
+	}
+}
